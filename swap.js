79f3e7432d5c920a9a97d7fd871f8fae9335edb4
miraz-rio <mirazbd99@gmail.com>
2024-06-08 00:22:29 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,14 @@
+var a = 5;
+var b = 8;
+// console.log("before swap: a=", a, "b=", b);
+var tamp = a;
+a = b;
+b = tamp;
+// console.log("after swap: a=", a, "b=", b);
+
+var x = 8;
+var y = 10;
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("after swap: x=", x, "y=", y);
