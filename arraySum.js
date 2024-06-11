7a26cec79a5dd5f7c56c4b9946ab54c79507f9b7
miraz-rio <mirazbd99@gmail.com>
2024-06-11 19:41:03 +0600

    arraySum

--- arraySum.js
+++ arraySum.js
@@ -1,7 +1,15 @@
-var marks = [10, 25, 25, 94, 78, 55, 94, 45];
+// Initialize the array with the given numbers
+var numbers = [10, 25, 25, 94, 78, 55, 94, 45];
+
+// Initialize the sum variable
 var sum = 0;
-for (var i = 0; i < marks.length; i++){
-    var element = marks[i];
+
+// Use a for loop to iterate through the array and calculate the sum
+for (var i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
     sum = sum + element;
+    // sum += numbers[i];
 }
-console.log("total of the number", sum);
+
+// Output the result
+console.log("The sum of the numbers is: ", sum);
